01ded5b1b388e849bdb7bbc692512763048a1b3d
Lukas Knoepfel <shylux@gmail.com>
2016-03-30 20:48:05 +0200

    wip

--- minmax.py
+++ minmax.py
@@ -1,9 +1,16 @@
+from sys import maxsize
+
+
 class Node(object):
 
     state = None
     children = []
+    max_node = False
+    parent = None
 
-    def __init__(self, state):
+    def __init__(self, parent, state):
+        self.parent = parent
+        self.max_node = not parent.max_node
         self.state = state
 
     def __repr__(self):
@@ -13,12 +20,40 @@ class Node(object):
             return "(" + ", ".join([str(child) for child in self.children]) + ")"
 
 
-def minmax(root_state, max_depth, expand_state, rate_state):
-    max_depth -= 1
+class MinMax(object):
+
+    root = None
+
+    def heuristic(self, state):
+        raise NotImplementedError()
+
+    def expand_state(self, state):
+        raise NotImplementedError()
+
+    def minimax(self, max_depth, node, alpha=0, beta=0):
+        max_depth -= 1
+
+        if not node:
+            node = self.root
+
+        if max_depth == 0:  # leaf node
+            return self.heuristic(node.state)
+
+        if len(node.children) == 0:
+            states = self.expand_state(node.state)
+            node.children = [Node(node, state) for state in states]
+
+        values = dict()
+        for child in node.children:
+            values[child] = self.minimax(child)
+
+        value = max(values) if node.max_node else min(values)
 
 
 
 testdata = [10, 11, 9, 12, 14, 15, 13, 14, 5, 2, 4, 1, 3, 22, 20, 21]
+
+
 def expand_tree(node):
     if len(node.state) > 1:
         n1 = Node(node.state[:len(node.state)/2])
