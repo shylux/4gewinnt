c04af8d1068272dfd3b07092229d5461263f8f66
tscheims <james.schuepbach@gmail.com>
2016-04-14 18:00:56 +0200

    updated build-scrhipt

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-cat starterbot.py minmax.py supibot.py > bot.py
+cat transposition.py starterbot.py minmax.py supibot.py > bot.py
 sed -i '/from minmax.*$/d' bot.py
 sed -i '/from starterbot.*$/d' bot.py
+sed -i '/from transposition.*$/d' bot.py
