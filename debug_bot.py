c7aa1f6d9e29ddc460b3aa66e131f97b0b491193
tscheims <james.schuepbach@gmail.com>
2016-04-14 11:51:51 +0200

    added board creator

--- debug_bot.py
+++ debug_bot.py
@@ -7,8 +7,17 @@ from supibot import SupiBot
 debug = debugHelper()
 debug.read_board_from_string('0,0,0,0,0,0,0;0,0,0,0,0,0,0;0,0,0,0,0,0,0;0,0,0,0,1,0,1;0,2,0,0,1,0,1;2,2,0,0,2,0,1')
 bot = SupiBot(True)
-bot.board = debug.board
+bot.board = debug.board.copy()
+bot.board[5,0] = 0
+bot.board[3,1] = 2
+print(bot.board)
+print(debug.board)
+print((bot.board-bot.board).all())
+print((bot.board-debug.board).all())
+print((str(bot.board) == str(debug.board)))
+print((str(bot.board) == str(bot.board)))
+
 bot.settings['your_botid'] = 2
 bot.settings['field_columns'] = 7
 bot.settings['field_rows'] = 6
-print(bot.make_turn())
+#print(bot.make_turn())
