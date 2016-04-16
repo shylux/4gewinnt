abd97b0362fe72ec97005c81c34a9f91e06b61ba
tscheims <james.schuepbach@gmail.com>
2016-04-17 01:01:31 +0200

    changed zobrist interface

--- zobrist_hash.py
+++ zobrist_hash.py
@@ -4,20 +4,19 @@ import numpy as np
 
 class zobristHash:
 
-    def __init__(self,states,x,y):
+    def __init__(self,states,size):
         self.states = states
-        self.x = x
-        self.y = y
-        self.map = np.zeros((x*y,states), dtype=np.int)    
+        self.size = size
+        self.map = np.zeros((size,states), dtype=np.int)    
     def precomupte(self):
         
-        for i in range(self.x*self.y):
+        for i in range(self.size):
             for y in range(self.states):
                 self.map[i][y] = randint(0,sys.maxsize)
     def calculate_hash(self,last_hash,position,state):
         return last_hash ^ self.map[position][state]
 
-zh = zobristHash(2,7,6)
+zh = zobristHash(2,42)
 zh.precomupte()
 print(zh.map)
 print(zh.calculate_hash(zh.calculate_hash(0,15,0),37,1))
