4131af1a2cadbfc16f30b0be8667c6de0e424345
tscheims <james.schuepbach@gmail.com>
2016-04-19 16:36:06 +0200

    optimized source code

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cat starterbot.py zobristhash.py transposition.py minmax.py supibot.py > bot.py
-sed -i '' '/from minmax.*$/d' bot.py
-sed -i '' '/from starterbot.*$/d' bot.py
-sed -i '' '/from transposition.*$/d' bot.py
-sed -i '' '/from zobristhash.*$/d' bot.py
+sed -i  '/from minmax.*$/d' bot.py
+sed -i  '/from starterbot.*$/d' bot.py
+sed -i  '/from transposition.*$/d' bot.py
+sed -i  '/from zobristhash.*$/d' bot.py
