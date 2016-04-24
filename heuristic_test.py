de7102236f7d5587899bcdc2eddea34e75219749
Shylux <shylux@gmail.com>
2016-04-24 13:35:23 +0200

    Updated heuristic test.

--- heuristic_test.py
+++ heuristic_test.py
@@ -15,34 +15,37 @@ bot.settings = {
     'field_columns': 7,
     'field_rows': 6
 }
-board = np.zeros((6, 7), dtype=np.uint8)
+board_null = np.zeros((6, 7), dtype=np.uint8)
 
 # test_line([1, 1, 0, 1, 0, 0, 0])
 # test_line([1, 0, 1, 1, 0, 0, 0])
 
-# test_state(board)
-#
-# board = bot.simulate_place_disc(board, 3, 1)
-# test_state(board)
-#
-# board = bot.simulate_place_disc(board, 3, 2)
-# test_state(board)
-#
-# board0 = bot.simulate_place_disc(board, 0, 1)
-# test_state(board0)
-# board1 = bot.simulate_place_disc(board, 1, 1)
-# test_state(board1)
-# board2 = bot.simulate_place_disc(board, 2, 1)
-# test_state(board2)
-#
-# board = bot.simulate_place_disc(board, 0, 1)
-# board = bot.simulate_place_disc(board, 1, 1)
-# test_state(board)
-#
-# board_win = bot.simulate_place_disc(board, 2, 1)
-# test_state(board_win)
+test_state(board_null)
 
-board = np.zeros((6, 7), dtype=np.uint8)
+board = bot.simulate_place_disc(board_null, 3, 1)
+test_state(board)
+
+board2 = bot.simulate_place_disc(board_null, 2, 1)
+test_state(board2)
+
+board = bot.simulate_place_disc(board, 3, 2)
+test_state(board)
+
+board0 = bot.simulate_place_disc(board, 0, 1)
+test_state(board0)
+board1 = bot.simulate_place_disc(board, 1, 1)
+test_state(board1)
+board2 = bot.simulate_place_disc(board, 2, 1)
+test_state(board2)
+
+board = bot.simulate_place_disc(board, 0, 1)
+board = bot.simulate_place_disc(board, 1, 1)
+test_state(board)
+
+board_win = bot.simulate_place_disc(board, 2, 1)
+test_state(board_win)
+
+board = board_null
 board = bot.simulate_place_disc(board, 0, 1)
 board = bot.simulate_place_disc(board, 0, 1)
 board = bot.simulate_place_disc(board, 1, 1)
